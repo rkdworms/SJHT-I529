d1c4e3148c57a24f565c5f24cfe4f62c4eb47f5b
rkdworms <rkdworms@gmail.com>
2022-10-18 11:01:40 +0900

    main_ver 0.0.7 로그인 에니메이션 추가 ERP 인사관리 프로젝트

--- src/main/java/sjht/erp/common/vo/MenuMasterVO.java
+++ src/main/java/sjht/erp/common/vo/MenuMasterVO.java
@@ -11,11 +11,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MenuMasterVO {
-    private Long menuid;
-    private UserMasterVO usertype;
-    private String menuname;
-    private String menuurl;
-    private Long groupnumber;
-    private Long odr;
-    private Long level;
+    private Long menuid; // PK
+    private UserMasterVO usertype; // 유저 권한
+    private String menuname; // 메뉴 이름
+    private String menuurl; // 메뉴 url
+    private Long groupnumber; // 메뉴 그룹
+    private Long odr; // 순서
+    private Long level; // 메뉴 층(대메뉴 소메뉴)
 }
